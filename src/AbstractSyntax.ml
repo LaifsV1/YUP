fd926825ff15d840e4454ea5f6a084743693c6fb
Yu-Yang Lin <yuyanglinhou@hotmail.com>
2015-07-04 21:11:02 +0100

    Updated Notes, Checker, AbstractSyntax
    Updated NOTES:
    - made proof rules clearer for implementation by adding p and q for OrR1
    and OrR2. (ASK ABOUT THIS)
    - added Left and Right constructors to OrR1 and OrR2.
    - added new section 3 for inference and checking.
    Updated CHECKER:
    - added proof checking function according to rules in NOTES.
    - added and-also function to make other functions shorter.
    - added comments to clear up all functions.
    - changed lookup function to lookup_ctx.
    - added new term check and replaced old term check with term inference
    according to rules in notes.
    - fixed application inference so it uses type checking on the argument.
    Had to change check_term to account for mutual recursion.
    Updated ABSTRACTSYNTAX:
    - added comments to make things clearer.
    - fixed proof abstract syntax for OrR1 and OrR2 to reflect NOTES.

--- src/AbstractSyntax.ml
+++ src/AbstractSyntax.ml
@@ -1,4 +1,5 @@
-(* Abstract Syntax Trees *)
+(* Abstract Syntax Trees - Syntax Grammar*)
+(* author: Yu-Yang Lin *)
 
 (* Variables *)
 type var = string 
@@ -7,20 +8,20 @@ type var = string
 type tp = Bool | Nat | List of tp | Arrow of tp * tp
 
 (* Terms *)
-type term = Var of var
-          | App of term * term
-          | Boolean of bool
+type term = Var of var                 (*var*)
+          | App of term * term         (*app*)
+          | Boolean of bool            (*bool*)
           | Zero | Suc of term         (*nats*)
           | Nil  | Cons of term * term (*lists*)
 
 (* Propositional Hypotheses *)
-type prop = Truth | Falsity 
-          | And of prop * prop 
-          | Or of prop * prop
-          | Implies of prop * prop
-          | Eq of term * term * tp
-          | Forall of var * tp * prop
-          | Exists of var * tp * prop
+type prop = Truth | Falsity            (*top and bot*)
+          | And of prop * prop         (*and*)
+          | Or of prop * prop          (*or*)
+          | Implies of prop * prop     (*implies*)
+          | Eq of term * term * tp     (*eq*)
+          | Forall of var * tp * prop  (*forall*)
+          | Exists of var * tp * prop  (*exists*)
 
 (* Term Context *)
 type ctx = ( var * tp ) list
@@ -32,9 +33,9 @@ type hyps = ( var * prop ) list
 type pf = AndL of (var * var) * var * pf        (*let (H',H'') = H in p*)
         | AndR of pf * pf                       (*p,q*)
         | OrL of var * (var * pf) * (var * pf)  (*match [H] with [H']:p | [H'']:q*)
-        | OrR1 of pf * pf                       (*A v B*)
-        | OrR2 of pf * pf                       (*A v B*)
-        | ImpliesL of pf * (var * var) * var    (*p, [H'] via [H], q*)
+        | OrR1 of pf                            (*A v B*)
+        | OrR2 of pf                            (*A v B*)
+        | ImpliesL of pf * (var * var) * pf     (*p, B [H'] via (A implies B) [H], q*)
         | ImpliesR of var * pf                  (*Assume [H], p*)
         | By of var                             (*by H*)
         | Therefore of pf * prop                (*p Therefore A*)
--- src/Checker.ml
+++ src/Checker.ml
@@ -1,52 +1,97 @@
+(* Well-Formnedness Checker Functions *)
+(* author: Yu-Yang Lin *)
+(* Check notes for specification of each function *)
 open AbstractSyntax
 
-(*lookup function for the context*)
-let rec lookup (psi : ctx) (x : var) :(tp option) = (try Some (List.assoc x psi) with Not_found -> None)
+(* lookup function for the context *)
+let rec lookup_ctx (psi : ctx) (x : var) :(tp option) = (try Some (List.assoc x psi) with Not_found -> None)
 
-(*term type checking*)
-let rec check_term (psi : ctx) (t : term) :(tp option) =
+(* lookup function for the hypotheses *)
+let rec lookup_hyps (gamma : hyps) (h : var) :(prop option) = (try Some (List.assoc h gamma) with Not_found -> None)
+
+(* and_also function as a shorthand *)
+let and_also r r' =
+  match r , r' with
+  | Some () , Some () -> Some ()
+  | _                 -> None
+
+(* term type inference [section 2 and 3 in notes], (psi |- t => tau) *)
+let rec infer_term (psi : ctx) (t : term) :(tp option) =
   match t with
   | Zero     -> Some Nat                                                                 (*nat-zero*)
-  | Suc (t') -> let tau = check_term psi t' in                                           (*nat-suc-n*)
-		(match tau with
-		 | Some Nat -> Some Nat
-		 | _        -> None)
-  | Var x        -> lookup psi x                                                         (*var*)
-  | App (t',t'') -> let (tau,tau') = (check_term psi t' , check_term psi t'') in         (*application*)
-		    (match tau,tau' with
-		     | Some (Arrow (a,b)) , Some a' -> if a = a' then Some b else None
-		     | _ -> None)
+  | Suc (t') -> (match infer_term psi t' with                                            (*nat-suc-n*)
+         | Some Nat -> Some Nat
+         | _        -> None)
+  | Var x        -> lookup_ctx psi x                                                     (*var*)
+  | App (e,v) -> (match infer_term psi e with                                            (*application*)
+                  | Some (Arrow (a,b)) -> (match check_term psi v a with
+                       | Some () -> Some b
+                       | _       -> None)                               
+          | _                  -> None)
   | Boolean (true)  -> Some Bool                                                         (*bool-true*)
   | Boolean (false) -> Some Bool                                                         (*bool-false*)
-  | Cons (t',t'')   -> let (tau,tau') = (check_term psi t' , check_term psi t'') in      (*list-hd::tl*)
-		       (match tau,tau' with
-			| Some tau, Some (List tau') -> if tau=tau' then Some (List tau) else None
-			| _ -> None)
-  | Nil -> assert false (*can't handle Nil yet.*)                                        (*list-empty*)
+  | Cons (v,v')     -> None                                                              (*list-hd::tl*)
+  | Nil             -> None                                                              (*list-empty*)
 
-(*proposition type checking*)
+(* term type checking [section 2 and 3 in notes], (psi |- t <= tau) *)
+and check_term (psi : ctx) (t : term) (tau : tp) :(unit option) =
+  match t , tau with
+  | Cons (v,v') , List tau'   -> and_also (check_term psi v tau')                       (*list-hd::tl*)
+                      (check_term psi v' (List tau'))
+  | Cons (v,v') , tau'        -> None
+  | Nil           , List tau' -> Some ()                                                 (*list-empty*)
+  | Nil           , tau'      -> None
+  | t'            , tau'      -> (match infer_term psi t' with                           (*inference case*)
+                  | Some tp'' -> if tp'' = tau' then (Some ()) else None
+                  | _         -> None)         
+
+(* proposition type checking [section 2 and 3 in notes] *)
 let rec check_prop (psi : ctx) (prop : prop) :(unit option) =
   match prop with
   | Truth   -> Some ()                                                                   (*top-prop*)
   | Falsity -> Some ()                                                                   (*bot-prop*)
-  | And (a,b)     -> let (a',b') = (check_prop psi a , check_prop psi b) in              (*and-prop*)
-		     (match a',b' with
-		      | Some (), Some () -> Some ()
-		      | _                -> None)
-  | Or (a,b)      -> let (a',b') = (check_prop psi a , check_prop psi b) in              (*or-prop*)
-		     (match a',b' with
-		      | Some (), Some () -> Some ()
-		      | _                -> None)
-  | Implies (a,b) -> let (a',b') = (check_prop psi a , check_prop psi b) in              (*implies-prop*)
-		     (match a',b' with
-		      | Some (), Some () -> Some ()
-		      | _                -> None)
-  | Eq (t,t',tau) -> let (tp',tp'') = (check_term psi t , check_term psi t') in          (*eq-prop*)
-		     (match tp',tp'' with
-		      | Some tau', Some tau'' -> if tau=tau' & tau=tau'' then Some () else None
-		      | _ -> None)
-  | Forall (x,tau,a) -> check_prop ((x,tau)::psi) a                                      (*forall-prop*)			
+  | And (a,b)     -> (match check_prop psi a , check_prop psi b with                     (*and-prop*)
+              | Some (), Some () -> Some ()
+              | _                -> None)
+  | Or (a,b)      -> (match check_prop psi a , check_prop psi b with                     (*or-prop*)
+              | Some (), Some () -> Some ()
+              | _                -> None)
+  | Implies (a,b) -> (match check_prop psi a , check_prop psi b with                     (*implies-prop*)
+              | Some (), Some () -> Some ()
+              | _                -> None)
+  | Eq (t,t',tau) -> (match check_term psi t tau , check_term psi t' tau with            (*eq-prop*)
+              | Some (), Some () -> Some ()
+              | _                -> None)
+  | Forall (x,tau,a) -> check_prop ((x,tau)::psi) a                                      (*forall-prop*)            
   | Exists (x,tau,a) -> check_prop ((x,tau)::psi) a                                      (*exists-prop*)
 
-(*proof type checking*)
-let rec check_pf (psi : ctx) (gamma : hyps) (proof : pf) (prop : prop) :(unit option) = Some ()
+(* entails function for By rule, can become more powerful this way *)
+let entails a b = (a = b)
+
+(* proof type checking [section 4 in notes] *)
+let rec check_pf (psi : ctx) (gamma : hyps) (proof : pf) (prop : prop) :(unit option) =
+  match proof , prop with
+  | AndR (p,q) , And (a,b)  -> and_also (check_pf psi gamma p a)                         (*AndR*)
+                    (check_pf psi gamma q b)
+  | AndR (p,q) , _          -> None
+  | AndL ((h,h'),h'',p) , c -> (match lookup_hyps gamma h'' with                         (*AndL*)
+                | Some (And (a,b)) -> check_pf psi ((h,a)::(h',b)::gamma) p c
+                | _                -> None)
+  | OrR1 p  , Or (a,b)         -> check_pf psi gamma p a                                 (*OrR_1*)
+  | OrR1 p  , _                -> None
+  | OrR2 q  , Or (a,b)         -> check_pf psi gamma q b                                 (*OrR_2*)
+  | OrR2 q  , _                -> None
+  | OrL (h,(h',p),(h'',q) ), c -> (match lookup_hyps gamma h with                        (*OrL*)
+                   | Some (Or (a,b)) -> and_also (check_pf psi ((h',a)::gamma)  p c)
+                                                 (check_pf psi ((h'',b)::gamma) q c)
+                                   | _               -> None)
+  | ImpliesR (h,p)        , Implies (a,b) -> check_pf psi ((h,a)::gamma) p b             (*ImpliesR*)
+  | ImpliesR (h,p)        , _             -> None
+  | ImpliesL (p,(h',h),q) , c             -> (match lookup_hyps gamma h with             (*ImpliesL*)
+                          | Some (Implies (a,b)) -> and_also (check_pf psi gamma           p a)
+                                         (check_pf psi ((h',b)::gamma) q c)
+                          | _                    -> None)
+  | By h , b -> (match lookup_hyps gamma h with                                          (*By*)
+         | Some a -> if entails a b then Some () else None
+         | None   -> None)
+  | Therefore (p,a) , b -> if a = b then check_pf psi gamma p b else None                (*Therefore*)
