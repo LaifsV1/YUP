3d0210c795f1bfd1994ce1b34e4655c5e1f6cc83
Yu-Yang Lin <yuyanglinhou@hotmail.com>
2015-07-21 05:08:37 +0100

    Updated Notes, CongClose
    Notes:
    -Updated rule for orientation, added another axiom to the premises.
    
    CongClose:
    -Fixed orientation rule to match notes. This seemingly fixed
    non-termination.
    -Simplified helper functions, no need for the extraction function as an
    argument.

--- src/CongruenceClosure.ml
+++ src/CongruenceClosure.ml
@@ -47,13 +47,12 @@ let rec find (ls : 'a list) (p : 'a -> bool) :(('a * 'a list) option) =
 let rec slide_find (xs : 'a list)             (* first list *)
                    (p : 'a -> 'c -> bool)     (* condition generator from first list *)
                    (ys : 'b list)             (* second list *)
-                   (q : 'b -> 'c)             (* extractor function for second list *)
         :(('a * 'a list * 'b * 'b list) option) =
   match xs with
   | []    -> None
-  | x::xs -> (match find ys (fun a -> (p x) (q a)) with
+  | x::xs -> (match find ys (fun a -> (p x) a) with
               | Some (z,zs) -> Some (x,xs,z,zs)
-              | None -> (match slide_find xs p ys q with
+              | None -> (match slide_find xs p ys with
                          | None -> None
                          | Some (x',xs',y',ys') -> Some (x',x::xs',y',ys')))
 
@@ -61,13 +60,12 @@ let rec slide_find (xs : 'a list)             (* first list *)
 (* conditions are generated for each element in the list, stopping when the condition is satified *)
 let rec compare_within (xs : 'a list)       (* list to compare within *)
                        (p : 'a -> 'b -> bool)  (* generate condition from first list *)
-                       (q: 'a -> 'b)           (* extract from list for comparison *)
         :(('a * 'a * 'a list) option) =     (* (first element, matching second element, list without first or second elements) *)
   match xs with
   | []    -> None
-  | x::xs -> (match find xs (fun a -> (p x) (q a)) with
+  | x::xs -> (match find xs (fun a -> (p x) a) with
               | Some (z,zs) -> Some (x,z,zs)
-              | None -> (match compare_within xs p q with
+              | None -> (match compare_within xs p with
                          | None -> None
                          | Some (x',y,ys) -> Some (x',y,x::ys)))
 
@@ -135,8 +133,7 @@ let rec sim ((k,e,(cs,ds)) : state) :(state option) =                           
   match slide_find ds (fun (t,c) -> fun (t',t'') ->
                                     (match expand_replace t' t (Var c) , expand_replace t'' t (Var c) with
                                      | None , None -> false
-                                     | _           -> true))
-                   e  (fun x -> x)
+                                     | _           -> true)) e
   with
   | None -> None
   | Some ((t,c) , ds' , (t',t'') , _) -> let e' = List.map (fun (a,b) ->
@@ -145,8 +142,11 @@ let rec sim ((k,e,(cs,ds)) : state) :(state option) =                           
                                          in Some (k, e' , (cs,ds))
 
 let rec ori ((k,e,(cs,ds)) : state) :(state option) =                                    (*Orientation Transition*)
-  match slide_find k (fun c -> fun t -> t=(Var c))
-                   e (fun (t,c) -> c)
+  match slide_find k (fun c -> fun (a,b) ->
+                               (match a,b with
+                                | Var t,Var c' -> c=c' && not (List.mem t k)
+                                | _    ,Var c' -> c=c'
+                                | _            -> false)) e
   with
   | None             -> None
   | Some (c,k',(t,Var c'),e') -> Some (c::k',e',(cs,(t,c')::ds))
@@ -158,25 +158,22 @@ let rec del ((k,e,r) : state) :(state option) =                                 
   | Some ((t,t'),e') -> Some (k,e',r)
 
 let rec ded ((k,e,(cs,ds)) : state) :(state option) =                                    (*Deduction Transition*)
-  match compare_within ds (fun (t,c) -> fun t' -> t=t')
-                       (fun (t,d) -> t)
+  match compare_within ds (fun (t,c) -> fun (t',d) -> t=t')
   with
   | None                    -> None
   | Some ((t',c),(t,d),ds') -> Some (k,(Var c, Var d)::e,(cs,(t,d)::ds'))
 
 let rec col ((k,e,(cs,ds)) : state) :(state option) =                                    (*Collapse Transition*)
-  match slide_find cs (fun (c,d) -> fun s ->
+  match slide_find cs (fun (c,d) -> fun (s,c') ->
                                     (match expand_replace s (Var c) (Var d) with
                                      | None -> false
-                                     | _    -> true))
-                   ds (fun (s,c') -> s)
+                                     | _    -> true)) ds
   with
   | None -> None
   | Some ((c,d),cs',(s,c'),ds') -> Some (k,e,(cs,(discard_expand_replace s (Var c) (Var d), c')::ds'))
 
 let rec com ((k,e,(cs,ds)) : state) :(state option) =                                    (*Composition Transition*)
-  match slide_find ds (fun (t,c) -> fun c' -> c=c')
-                   cs (fun (c,d) -> c)
+  match slide_find ds (fun (t,c) -> fun (c',d) -> c=c') cs
   with
   | None                        -> None
   | Some ((t,c),ds',(c',d),cs') -> Some (k,e,((c',d)::cs',(t, d)::ds'))
@@ -184,13 +181,13 @@ let rec com ((k,e,(cs,ds)) : state) :(state option) =                           
 
 (******** BUILDING THE CLOSURE ********)
 let rec build_closure' (sigma : state) :(state option) =
-  let step =   or_else (ext sigma)
-                       (or_else (sim sigma)
-                                (or_else (ori sigma)
-                                         (or_else (del sigma)
-                                                  (or_else (ded sigma)
-                                                           (or_else (col sigma)
-                                                                    (com sigma))))))
+  let step = or_else (ext sigma)
+                     (or_else (sim sigma)
+                              (or_else (ori sigma)
+                                       (or_else (del sigma)
+                                                (or_else (ded sigma)
+                                                         (or_else (col sigma)
+                                                                  (com sigma))))))
   in (match step with
       | None   -> Some sigma
       | Some s -> build_closure' s)
@@ -199,10 +196,11 @@ let build_closure (e : e_set) :(state option) = build_closure' ([],e,([],[]))
 
 (*for testing*)
 let step_through (sigma : state) :(state option) =
-  or_else (ext sigma)
-          (or_else (sim sigma)
-                   (or_else (ori sigma)
-                            (or_else (del sigma)
-                                     (or_else (ded sigma)
-                                              (or_else (col sigma)
-                                                       (com sigma))))))
+  let step = or_else (ext sigma)
+                     (or_else (sim sigma)
+                              (or_else (ori sigma)
+                                       (or_else (del sigma)
+                                                (or_else (ded sigma)
+                                                         (or_else (col sigma)
+                                                                  (com sigma))))))
+  in step
