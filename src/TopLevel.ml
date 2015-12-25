8645a600ecc159f5f7cb2279e4b7c2c3df571070
Kelsey McKenna <ToastNumber@users.noreply.github.com>
2015-12-25 00:08:46 +0000

    Fixed typo
    Useage -> Usage

--- src/TopLevel.ml
+++ src/TopLevel.ml
@@ -89,7 +89,7 @@ let _ =
                      print_newline ();
                      exit 1
   | e -> Printf.eprintf "    [UNEXPECTED EXCEPTION] : %s \n" (Printexc.to_string e);
-         Printf.eprintf "    Useage: \n";
+         Printf.eprintf "    Usage: \n";
          Printf.eprintf "    Windows: .\\yup.exe <file path> \n";
          Printf.eprintf "    Unix:    ./yup.exe <file path> \n";
          Printf.eprintf "    for version number: ./yup.exe --version \n";
